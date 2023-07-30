2b2b3ac08d76d16234c76d845308d0e8522b3107
Alex <ssaasshha11@yandex.ru>
2023-07-30 10:16:12 +0400

    upd

--- task3/src/main/java/task3/Main.java
+++ task3/src/main/java/task3/Main.java
@@ -4,24 +4,45 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
         List<TestEntity> tests = GetTests(args[0]);
-        List<ValueEntity> values = GetValues(args[1]);
+        Map<Integer, String> values = GetValues(args[1]);
 
         flatten(tests).forEach(t -> {
-            values.stream()
-                    .filter(v -> v.getId() == t.getId())
-                    .findFirst().ifPresent(v -> t.setValue(v.getValue()));
+            if(values.containsKey(t.getId()))
+                t.setValue(values.get(t.getId()));
         });
+
+        String filePath = Paths.get(getParentFolder(args[0]), "report.json").toString();
         Map<String, List<TestEntity>> map = new TreeMap<String, List<TestEntity>>(){{put("tests", tests);}};
-        System.out.println(new Gson().toJson(map));
+        String content = new Gson().toJson(map);
+
+        writeToFile(filePath, content);
+    }
+
+    private static void writeToFile(String filePath, String content) {
+        try (Writer writer = new BufferedWriter(
+                new OutputStreamWriter(
+                        Files.newOutputStream(Paths.get(filePath)), StandardCharsets.UTF_8))
+        ) {
+            writer.write(content);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String getParentFolder(String path) {
+        File file = new File(path);
+        return file.getAbsoluteFile().getParent().toString();
     }
 
     private static List<TestEntity> flatten(List<TestEntity> test) {
@@ -55,13 +76,17 @@ public class Main {
         return tests.get("tests");
     }
 
-    private static ArrayList<ValueEntity> GetValues(String valuesPath) throws FileNotFoundException {
+    private static Map<Integer, String> GetValues(String valuesPath) throws FileNotFoundException {
         Gson gson = new Gson();
         JsonReader valuesReader = new JsonReader(new FileReader(valuesPath));
 
         Type valuesList = new TypeToken<Map<String, ArrayList<ValueEntity>>>(){}.getType();
         Map<String, ArrayList<ValueEntity>> values = gson.fromJson(valuesReader, valuesList);
 
-        return values.get("values");
+        Map<Integer, String> dict = new HashMap<>();
+        values.get("values")
+                .forEach(v -> dict.put(v.getId(), v.getValue()));
+
+        return dict;
     }
 }
